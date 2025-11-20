746e537149fa3e3192c81804d52914c23b8363ea
DevMa7e1 <thedevma7e1@gmail.com>
2025-11-20 19:06:17 +0200

    Add calibration + mouth opening detection
    I literally just got an idea on how to improve the algorithm and make it usable but I have to make a commit first...

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import cv2
 import mediapipe as mp
 import time
+from tkinter import *
+
+tk = Tk()
+tk.title = "Program"
+tk.resizable(0,0)
 
 MODEL_PATH = "face_landmarker.task"
 
@@ -14,9 +19,27 @@ options = FaceLandmarkerOptions(
     running_mode=VisionRunningMode.VIDEO,
     num_faces=1
 )
+okpressed = False
+def okcmd():
+    global okpressed
+    okpressed = True
 
 def main():
+    global okpressed, okbt, okcmd
     cap = cv2.VideoCapture(0)
+    te = StringVar()
+    te.set("")
+    text = Label(tk, textvariable=te)
+    text.pack()
+    okbt = Button(tk, text="OK", command=okcmd)
+    okbt.pack()
+    fohfr = 0
+    calr = 0
+    calcr = 0
+    call = 0
+    calcl = 0
+    calm = 0
+    calom = 0
     with FaceLandmarker.create_from_options(options) as landmarker:
         while True:
             ret, frame = cap.read()
@@ -36,6 +59,8 @@ def main():
                 reye_u = (0, 0)
                 leye_l = (0, 0)
                 leye_u = (0, 0)
+                mout_l = (0, 0)
+                mout_u = (0, 0)
                 for p in landmarks:
                     i += 1
                     x = int(p.x * frame.shape[1])
@@ -48,23 +73,65 @@ def main():
                         leye_u = (x, y)
                     if(i == 374):
                         reye_l = (x, y)
+                    if(i == 82):
+                        mout_u = (x, y)
+                    if(i == 88):
+                        mout_l = (x, y)
                     cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
                 dr = abs(reye_u[1] - reye_l[1])+1
                 dl = abs(leye_u[1] - leye_l[1])+1
-                print(dl, dr)
-                if 1/dl > 1/10:
-                    print("L-", end=" ")
+                dm = abs(mout_u[1] - mout_l[1])+1
+                print(dl, dr, okpressed)
+                t = ""
+                if fohfr > 200:
+                    if abs(dr-calcl) > call-calcl:
+                        t += "L- " 
+                    else:
+                        t += "LO "
+                    if abs(dr-calcr) > calr-calcr:
+                        t += "R- "
+                    else:
+                        t += "RO "
+                    if 1/dm > 1/calm :
+                        t += "M- "
+                    else:
+                        t += "MO "
+                if(fohfr < 100):
+                    te.set(f"Keep both eyes open and mouth closed. Stay in a fixed, comfortable position. Press OK to start. Calibrating for {str(fohfr)}/100 frames... Values: " + str(calr) + " " + str(call) + " " + str(calm))
+                    calr = round((calr+dr)/2)
+                    call = round((call+dl)/2)
+                    calm = round((calm+dm)/2)
+                    if okpressed:
+                        fohfr += 1
+                elif fohfr == 100:
+                    tk.bell()
+                    okpressed = False
+                    calr -= 0
+                    call -= 0
+                    calm += 2
+                    calr = calr
+                    call = call
+                    calm = calm
+                    fohfr += 1
+                elif fohfr < 201:
+                    te.set(f"Keep both eyes closed. Stay in a fixed, comfortable position. Press OK to start. Calibrating for {str(fohfr)}/201 frames... Values: " + str(calcr) + " " + str(calcl) + " " + str(calom))
+                    calcr = round((calr+dr)/2)
+                    calcl = round((call+dl)/2)
+                    calom = round((calm+dm)/2)
+                    if okpressed:
+                        fohfr += 1
+                elif fohfr == 201:
+                    okbt.destroy()
+                    okpressed = False
+                    tk.bell()
+                    fohfr += 1
                 else:
-                    print("LO", end=" ")
-                if 1/dr > 1/20:
-                    print("R-", end=" ")
-                else:
-                    print("RO", end=" ")
-                print()
-                
+                    te.set(t + f" L: {call},{calcl} R: {calr},{calcr} M: {calm}")
             cv2.imshow("Face Landmarks", frame)
             if cv2.waitKey(1) & 0xFF == 27:
                 break
+            tk.update_idletasks()
+            tk.update()
 
     cap.release()
     cv2.destroyAllWindows()
